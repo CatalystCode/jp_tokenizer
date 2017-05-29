4e941290aca6824a600093118209ddbec093e4fb
Clemens Wolff <clewolff@microsoft.com>
2017-05-29 12:15:58 +0000

    Make sure to install correct version of python-dev

--- setup_server.sh
+++ setup_server.sh
@@ -2,7 +2,7 @@
 
 sudo add-apt-repository -y ppa:jonathonf/python-3.6
 sudo apt-get update
-sudo apt-get install -y python3.6 python3-pip python3-dev build-essential
+sudo apt-get install -y python3.6 python3-pip python3.6-dev build-essential
 
 sudo apt-get install -y mecab mecab-ipadic libmecab-dev mecab-ipadic-utf8
 sudo apt-get install -y git
