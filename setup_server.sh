ffdfe6f2b85a1f33e4e937699192ca2d3b293b1d
Clemens Wolff <clewolff@microsoft.com>
2017-05-29 13:04:38 +0000

    Add documentation

--- setup_server.sh
+++ setup_server.sh
@@ -1,22 +1,26 @@
 #!/usr/bin/env bash
 
+# install python
 sudo apt-get install -y python3.5 python3-pip python3.5-dev build-essential
 pip3 install --upgrade pip wheel
 
+# install mecab tokenizer
 sudo apt-get install -y mecab mecab-ipadic libmecab-dev mecab-ipadic-utf8
 sudo apt-get install -y git
-
 git clone --depth 1 https://github.com/neologd/mecab-ipadic-neologd.git
 (cd mecab-ipadic-neologd; sudo ./bin/install-mecab-ipadic-neologd -n -y)
 
+# install service
 git clone --depth 1 https://github.com/CatalystCode/jp_tokenizer.git
 (cd jp_tokenizer; pip3 install -r requirements.txt)
 
+# enable service to run on port 80
 sudo apt-get install -y authbind
 sudo touch /etc/authbind/byport/80
 sudo chown $USER:$USER /etc/authbind/byport/80
 sudo chmod 755 /etc/authbind/byport/80
 
+# auto-start service
 sudo apt-get install -y supervisor
 sudo service supervisor start
 sudo tee /etc/supervisor/conf.d/jp_tokenizer.conf << EOF
