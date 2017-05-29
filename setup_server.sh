654abd27e798258ffcabcbebf34f77ad1090b5a8
Clemens Wolff <clewolff@microsoft.com>
2017-05-29 04:51:56 +0000

    Force non-interactive mode

--- setup_server.sh
+++ setup_server.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-sudo add-apt-repository ppa:jonathonf/python-3.6
+sudo add-apt-repository -y ppa:jonathonf/python-3.6
 sudo apt-get update
-sudo apt-get install python3.6 python3-pip
+sudo apt-get install -y python3.6 python3-pip
 
 sudo apt-get install -y mecab mecab-ipadic libmecab-dev mecab-ipadic-utf8
 sudo apt-get install -y git
 
 git clone --depth 1 https://github.com/neologd/mecab-ipadic-neologd.git
-(cd mecab-ipadic-neologd; sudo ./bin/install-mecab-ipadic-neologd -n)
+(cd mecab-ipadic-neologd; sudo ./bin/install-mecab-ipadic-neologd -n -y)
 
 pip3 install mecab-python3 sanic
