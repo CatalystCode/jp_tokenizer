ca68ea52cc6d45c6d8ed7be6cb13273558c2348f
Clemens Wolff <clewolff@microsoft.com>
2017-05-29 11:53:24 +0000

    Split out requirements file

--- setup_server.sh
+++ setup_server.sh
@@ -2,7 +2,7 @@
 
 sudo add-apt-repository -y ppa:jonathonf/python-3.6
 sudo apt-get update
-sudo apt-get install -y python3.6 python3-pip
+sudo apt-get install -y python3.6 python3-pip python3-dev build-essential
 
 sudo apt-get install -y mecab mecab-ipadic libmecab-dev mecab-ipadic-utf8
 sudo apt-get install -y git
@@ -10,4 +10,4 @@ sudo apt-get install -y git
 git clone --depth 1 https://github.com/neologd/mecab-ipadic-neologd.git
 (cd mecab-ipadic-neologd; sudo ./bin/install-mecab-ipadic-neologd -n -y)
 
-pip3 install mecab-python3 sanic
+pip3 install -r requirements.txt
