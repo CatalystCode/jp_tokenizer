67261b358c9231d7be6e3414724bcde9e2d7e03e
Clemens Wolff <clewolff@microsoft.com>
2017-05-29 12:19:50 +0000

    Avoid relying on python 3.6

--- setup_server.sh
+++ setup_server.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-sudo add-apt-repository -y ppa:jonathonf/python-3.6
-sudo apt-get update
-sudo apt-get install -y python3.6 python3-pip python3.6-dev build-essential
+sudo apt-get install -y python3.5 python3-pip python3.5-dev build-essential
 
 sudo apt-get install -y mecab mecab-ipadic libmecab-dev mecab-ipadic-utf8
 sudo apt-get install -y git
