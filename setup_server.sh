e6f0b7433c9574ec4050267cceef0a67cdd577cc
Clemens Wolff <clewolff@microsoft.com>
2017-05-29 13:42:10 +0000

    Parameterize setup script

--- setup_server.sh
+++ setup_server.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
-# install python
-sudo apt-get install -y python3.5 python3.5-venv python3.5-dev build-essential
+set -euo pipefail
+
+github_account='CatalystCode'
+github_repo='jp_tokenizer'
+runcmd='run_server.py'
+runas="$USER"
+rootdir="$PWD"
+python='python3.5'
 
 # install mecab tokenizer
 sudo apt-get install -y mecab mecab-ipadic libmecab-dev mecab-ipadic-utf8
@@ -9,31 +15,40 @@ sudo apt-get install -y git
 git clone --depth 1 https://github.com/neologd/mecab-ipadic-neologd.git
 (cd mecab-ipadic-neologd; sudo ./bin/install-mecab-ipadic-neologd -n -y)
 
-# install service
-python3.5 -m venv jp_tokenizer-env
-jp_tokenizer-env/bin/pip install -U pip wheel
-git clone --depth 1 https://github.com/CatalystCode/jp_tokenizer.git
-jp_tokenizer-env/bin/pip install -r jp_tokenizer/requirements.txt
+# install python
+sudo apt-get install -y "${python}" "${python}-venv" "${python}-dev" build-essential
 
-# enable service to run on port 80
+# install service
+rootdir="${rootdir}/${github_account}-${github_repo}"
+mkdir -p "${rootdir}"
+venv="${rootdir}/env"
+repo="${rootdir}/src"
+"${python}" -m venv "${venv}"
+pip="${venv}/bin/pip"
+python="${venv}/bin/python"
+"${pip}" install -U pip wheel
+git clone --depth 1 "https://github.com/${github_account}/${github_repo}.git" "${repo}"
+"${pip}" install -r "${repo}/requirements.txt"
+
+# allow service to run on port 80
 sudo apt-get install -y authbind
-sudo touch /etc/authbind/byport/80
-sudo chown $USER:$USER /etc/authbind/byport/80
-sudo chmod 755 /etc/authbind/byport/80
+sudo touch '/etc/authbind/byport/80'
+sudo chown "$runas:$runas" '/etc/authbind/byport/80'
+sudo chmod 755 '/etc/authbind/byport/80'
 
 # auto-start service
+daemon="$(basename ${rootdir})"
 sudo apt-get install -y supervisor
 sudo service supervisor start
-sudo tee /etc/supervisor/conf.d/jp_tokenizer.conf << EOF
-[program:jp_tokenizer]
-command=/usr/bin/authbind $PWD/jp_tokenizer-env/bin/python $PWD/jp_tokenizer/run_server.py
-directory=/tmp
+sudo tee "/etc/supervisor/conf.d/${daemon}.conf" << EOF
+[program:${daemon}]
+command=/usr/bin/authbind '${python}' '${repo}/${runcmd}'
 autostart=true
 autorestart=true
 startretries=3
-stderr_logfile=/var/log/jp_tokenizer.err.log
-stdout_logfile=/var/log/jp_tokenizer.out.log
-user=$USER
+stderr_logfile=/var/log/${daemon}.err.log
+stdout_logfile=/var/log/${daemon}.out.log
+user=$runas
 environment=
 EOF
 sudo supervisorctl reread
