9435b215a73ba685972c6972c1c63f3eea67a966
Clemens Wolff <clewolff@microsoft.com>
2017-05-29 04:51:38 +0000

    Avoid changing working directory

--- setup_server.sh
+++ setup_server.sh
@@ -8,7 +8,6 @@ sudo apt-get install -y mecab mecab-ipadic libmecab-dev mecab-ipadic-utf8
 sudo apt-get install -y git
 
 git clone --depth 1 https://github.com/neologd/mecab-ipadic-neologd.git
-cd mecab-ipadic-neologd
-sudo ./bin/install-mecab-ipadic-neologd -n
+(cd mecab-ipadic-neologd; sudo ./bin/install-mecab-ipadic-neologd -n)
 
 pip3 install mecab-python3 sanic
